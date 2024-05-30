db28342f8aeffbaa0c2c8373798f587d7ca4a95f
hun <intelhsh@naver.com>
2024-05-30 11:49:51 +0900

    Revert "test 환경 set 및 일부 구현"
    This reverts commit 26d8063911f822baa030d88b56f213a2af64e6f8.

--- Project15/prime-factors.cpp
+++ Project15/prime-factors.cpp
@@ -1,15 +1,3 @@
-#include<vector>
-using std::vector;
-
 class PrimeFactor {
-public:
-	vector<int> of(int i) {
-		vector<int> result = {};
-		
-		if (i == 2) {
-			result.push_back(2);
-		}
-		
-		return result;
-	}
+
 };
--- Sample-Test1/prime-factors_test.cpp
+++ Sample-Test1/prime-factors_test.cpp
@@ -3,19 +3,6 @@
 
 using namespace std;
 
-
-class PrimeTestFixture : public testing::Test {
-public:
-	PrimeFactor prime_factor;
-	vector<int> expected;
-};
-
-TEST_F(PrimeTestFixture, PrimeTest1) {
-	expected = {};
-	EXPECT_EQ(expected, prime_factor.of(1));
-}
-
-TEST_F(PrimeTestFixture, PrimeTest2) {
-	expected = {2};
-	EXPECT_EQ(expected, prime_factor.of(2));
+TEST(PrimeFactors, PrimeTest) {
+  EXPECT_EQ(1, 1);
 }
