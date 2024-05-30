26d8063911f822baa030d88b56f213a2af64e6f8
hun <intelhsh@naver.com>
2024-05-30 11:49:42 +0900

    test 환경 set 및 일부 구현

--- Project15/prime-factors.cpp
+++ Project15/prime-factors.cpp
@@ -1,3 +1,15 @@
-class PrimeFactor {
+#include<vector>
+using std::vector;
 
+class PrimeFactor {
+public:
+	vector<int> of(int i) {
+		vector<int> result = {};
+		
+		if (i == 2) {
+			result.push_back(2);
+		}
+		
+		return result;
+	}
 };
